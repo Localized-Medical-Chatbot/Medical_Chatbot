bae576e563bc90a0f4bf364d857483b34189d679
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2023-08-25 12:20:42 +0530

    database connection

--- actions/actions.py
+++ actions/actions.py
@@ -12,6 +12,57 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+import mysql.connector
+class DatabaseConnection:
+    hostname = None
+    database = None
+    username = None
+    password = None
+    connection = None
+    cursor = None
+    query = None
+
+    def __init__(self, hostname, database, username, password):
+        if not self.connection:
+            self.hostname = hostname
+            self.database = database
+            self.username = username
+            self.password = password
+            self.connect()
+
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host     = self.hostname,
+            user     = self.username,
+            password = self.password,
+            database = self.database)
+        
+        return self.connection
+
+    def disconnect(self):
+        self.cursor.close()
+        self.connection.close()
+    
+    def query(self, query):
+        result = []
+
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(query)
+
+        for row in self.cursor:
+            result.append(row)
+
+        return result
+
+    def count(self, table, condition = None):
+        return len(self.simple_query(table, '*', condition))
+
+# set your details here
+DATABASE_NAME = 'hospitaldb'
+USERNAME = 'root'
+PASSWORD = None # set your password 
+HOST = 'LOCALHOST'
+
 
 class ActionHelloWorld(Action):
 
@@ -25,3 +76,21 @@ class ActionHelloWorld(Action):
         dispatcher.utter_message(text="Hello World!")
 
         return []
+    
+class ActionGetDoctorName(Action):
+
+    def name(self) -> Text:
+        return "action_get_doctor_name"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        db = DatabaseConnection(HOST, DATABASE_NAME, USERNAME, PASSWORD)
+        results = db.query("SELECT first_name FROM doctor WHERE first_name = 'Jennifer';")
+        # developer_name = results[0]
+        results = str(results)
+        db.disconnect()
+        
+        dispatcher.utter_message(text=results)
+        return[]
